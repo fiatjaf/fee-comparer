065a21be2c564f97d797947541dde2a959004c87
fiatjaf <fiatjaf@alhur.es>
2020-05-26 23:01:16 -0300

    measures to improve speed.

--- getdata/helpers.py
+++ getdata/helpers.py
@@ -6,3 +6,7 @@ BTC = 100_000_000
 def get_txo_amount(txid, output_n):
     tx = bitcoin.getrawtransaction(txid, True)
     return int(tx["vout"][output_n]["value"] * BTC)
+
+
+def normalize_value(value):
+    return round(round(value ** (1 / 4.5)) ** 4.5)
--- getdata/run_day.py
+++ getdata/run_day.py
@@ -1,21 +1,19 @@
 import random
 import datetime
 import requests
-from multiprocessing import Pool
-from statistics import mean, quantiles
+from multiprocessing import Pool, Manager
+from statistics import quantiles
 from pprint import pprint as pp
 
 from .globals import bitcoin, SPARK_URL, SPARK_TOKEN
-from .helpers import get_txo_amount, BTC
+from .helpers import get_txo_amount, BTC, normalize_value
 
 # get a list of nodes
-r = requests.post(
-    SPARK_URL,
-    headers={"X-Access": SPARK_TOKEN},
-    json={"method": "listnodes"},
-    verify=False,
+r = requests.get(
+    "https://ln.bigsun.xyz/api/nodes?select=pubkey&order=openchannels.desc",
+    headers={"Range": "250-1250"},
 )
-nodes = [node["nodeid"] for node in r.json()["nodes"]]
+nodes = [item["pubkey"] for item in r.json()]
 
 
 def run_day(db):
@@ -36,7 +34,6 @@ def run_day(db):
     # get the list of blocks we're interested in
     blocks_for_the_day = []
     for block_number in range(scan_since, tip):
-        print("block", block_number)
         block_hash = bitcoin.getblockhash(block_number)
         block = bitcoin.getblock(block_hash)
         block_date = datetime.datetime.fromtimestamp(block["time"]).date()
@@ -48,16 +45,22 @@ def run_day(db):
         # here we know we're in the correct day
         blocks_for_the_day.append(block_hash)
 
-    print(f"found {len(blocks_for_the_day)} blocks")
-    pool = Pool(processes=8)
-    results = pool.imap_unordered(run_block, blocks_for_the_day)
-
     total_payments = []
     overpaid_payments = []
 
-    for total, overpaid in results:
-        total_payments += total
-        overpaid_payments += overpaid
+    print(
+        f"found {len(blocks_for_the_day)} blocks from {blocks_for_the_day[0]} to {blocks_for_the_day[-1]}"
+    )
+    with Manager() as manager:
+        global ln_fee_cache
+        ln_fee_cache = manager.dict()
+
+        with Pool(processes=12) as pool:
+            results = pool.imap_unordered(run_block, blocks_for_the_day)
+
+            for total, overpaid in results:
+                total_payments += total
+                overpaid_payments += overpaid
 
     # reached the end, calculate values for the day
     pp(
@@ -108,7 +111,10 @@ def run_block(block_hash):
     total_payments = []
     overpaid_payments = []
 
-    txs = bitcoin.getblock(block_hash, 2)["tx"][1:]  # exclude coinbase
+    block_data = bitcoin.getblock(block_hash, 2)
+    print(block_data["height"])
+
+    txs = block_data["tx"][1:]  # exclude coinbase
     for tx in txs:
         # exclude bizarre transactions, coinjoin and so on
         if len(tx["vout"]) > 50:
@@ -123,53 +129,92 @@ def run_block(block_hash):
         if outputsum == 0:
             continue
 
-        # exclude very obvious change outputs
         vouts = []
         for vout in tx["vout"]:
             sat = int(vout["value"] * BTC)
-            if len(tx["vout"]) > 1 and outputsum > 100000 and (sat / outputsum) > 0.90:
-                print("  excluding change ", sat, outputsum)
+
+            # exclude zero or very small outputs
+            if sat < 100:
                 continue
+
+            # exclude very obvious change outputs
+            if len(tx["vout"]) > 1 and outputsum > 100000 and (sat / outputsum) > 0.90:
+                continue
+
             vouts.append(vout)
 
+        if len(vouts) == 0:
+            continue
+
         # the fee for each payment is the tx fee divided by the number of payments
-        chain_fee = total_fee / len(vouts)
+        chain_fee = int(total_fee / len(vouts))
 
         # each vout is treated as a different payment, total fee is splitted
         for vout in vouts:
             sat = int(vout["value"] * BTC)
+            sat_normalized = normalize_value(sat)
+
             total_payments.append(sat)
 
-            # estimate lightning fee by calculating random routes to 3 destinations
-            ln_fees = []
-            for i in range(7):
-                target = random.choice(nodes)
-                r = requests.post(
-                    SPARK_URL,
-                    headers={"X-Access": SPARK_TOKEN},
-                    json={"method": "getroute", "params": [target, f"{sat}sat", 0],},
-                    verify=False,
-                )
-                if not r.ok:
+            ln_fee = None
+
+            # check cache
+            ln_fee = ln_fee_cache.get(sat_normalized)
+            if not ln_fee:
+                # estimate lightning fee by calculating random routes to 3 destinations
+                ln_fees = []
+                for i in range(7):
+                    target = random.choice(nodes)
+                    r = requests.post(
+                        SPARK_URL,
+                        headers={"X-Access": SPARK_TOKEN},
+                        json={
+                            "method": "getroute",
+                            "params": {
+                                "id": target,
+                                "msatoshi": f"{sat_normalized}sat",
+                                "riskfactor": 5,
+                            },
+                        },
+                        verify=False,
+                    )
+                    if not r.ok:
+                        continue
+
+                    fee_msat = r.json()["route"][0]["msatoshi"] - (
+                        sat_normalized * 1000
+                    )
+                    if fee_msat == 0:
+                        continue
+
+                    print(f"    estimated_fee ({sat_normalized}): {fee_msat}")
+                    ln_fees.append(fee_msat)
+
+                    if len(ln_fees) >= 3:
+                        break
+
+                if not ln_fees:
+                    # didn't find any route, so forget about this payment
+                    print("    no route: ", sat, chain_fee)
+                    ln_fee_cache[sat_normalized] = False
                     continue
 
-                fee_msat = r.json()["route"][0]["msatoshi"] - (sat * 1000)
-                ln_fees.append(fee_msat)
+                ln_fee_msat = max(ln_fees)
+                ln_fee = int(ln_fee_msat / 1000)
+                ln_fee_cache[sat_normalized] = ln_fee
 
-                if len(ln_fees) >= 3:
-                    break
-
-            if not ln_fees:
-                # didn't find any route, so forget about this payment
-                continue
-
-            ln_fee = mean(ln_fees)
+            # denormalize
+            ln_fee = ln_fee * sat / sat_normalized
 
             print(
-                f"{tx['txid']}:{vout['n']}", sat, chain_fee, int(ln_fee / 1000),
+                block_data["height"],
+                f"{tx['txid']}:{vout['n']}",
+                sat,
+                chain_fee,
+                ln_fee,
             )
 
-            if ln_fee / 1000 < chain_fee:
+            if ln_fee < chain_fee:
                 overpaid_payments.append((sat, chain_fee, ln_fee))
 
     return total_payments, overpaid_payments
